0947daa48c3a8835260315a961d447f42f9c01e3
Dipto Das <dipto2512@gmail.com>
2025-05-15 22:10:00 +0600

    fastPost

---
+++ BlogNest.Presentation/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BlogNest.Presentation.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogNest.Presentation.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> a) : base(a)
+        {
+
+        }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+
+    }
+}
